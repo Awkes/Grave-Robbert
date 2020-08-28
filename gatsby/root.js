780d0a052bd2be784eb3bb7d58e83c22683359e3
Andreas Åkerlöf <a.akerlof@gmail.com>
2020-08-28 13:30:42 +0200

    feat(theme-ui): added initial theme values

---
+++ gatsby/root.js
@@ -0,0 +1,27 @@
+import 'typeface-reenie-beanie';
+import 'typeface-assistant';
+import { css, Global } from '@emotion/core';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+export const wrapRootElement = ({ element }) => (
+  <>
+    <Global
+      styles={css`
+        :root {
+          scroll-behavior: smooth;
+          min-width: 320px;
+        }
+
+        body {
+          margin: 0;
+        }
+      `}
+    />
+    {element}
+  </>
+);
+
+wrapRootElement.propTypes = {
+  element: PropTypes.node.isRequired,
+};
---
+++ src/gatsby-plugin-theme-ui/index.js
@@ -0,0 +1,33 @@
+export default {
+  colors: {
+    primary: '#C6D62D',
+    secondary: 'rgba(96,63,112,0.7)',
+    tertiary: 'rgba(97,76,107,0.7)',
+    text: '#fff',
+    background: 'rgba(0,0,0,0.8)',
+    muted: '#000',
+  },
+  fonts: {
+    body: 'Assistant',
+    heading: 'Reenie Beanie',
+    button: 'Reenie Beanie',
+  },
+  fontSizes: [
+    12, // 0
+    14, // 1
+    16, // 2
+    20, // 3
+    30, // 4
+    40, // 5
+  ],
+  space: [
+    0, // 0
+    5, // 1
+    10, // 2
+    15, // 3
+    20, // 4
+    25, // 5
+    30, // 6
+    50, // 7
+  ],
+}
