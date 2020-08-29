9093a697d6f6a0f7a6f7f7bee5a7d19385a83e47
Andreas Åkerlöf <a.akerlof@gmail.com>
2020-08-29 15:20:09 +0200

    feat(partners): partner section

---
+++ src/components/Partners.js
@@ -0,0 +1,47 @@
+/** @jsx jsx */
+import PropTypes from 'prop-types';
+import { jsx } from 'theme-ui';
+
+const Partners = ({ links }) => (
+  <section sx={{ backgroundColor: 'secondary', paddingY: 5, borderTop: 0, borderBottom: 0 }}>
+    <ul 
+      sx={{ 
+        listStyleType: 'none', 
+        margin: 0, 
+        padding: 0, 
+        textAlign: 'center',
+      }}
+    >
+      {links.map(({ title, link, id, image }) => (
+        <li 
+          key={id}
+          sx={{ 
+            display: 'inline-block',
+            paddingX: 3,
+            paddingY: 2,
+          }}
+        >
+            <a 
+              href={link} 
+              rel="noreferrer noopener" 
+              target="_blank"
+              >
+              <img src={image} alt={title} sx={{ height: '40px', maxWidth: '100%' }} />
+            </a>
+          </li>
+      ))}
+    </ul>
+  </section>
+);
+
+export default Partners;
+
+Partners.propTypes = {
+  links: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      link: PropTypes.string,
+      id: PropTypes.string,
+    })
+  ).isRequired,
+};
