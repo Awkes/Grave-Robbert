7a4984fa20139eca7095df296407cb8e58e8aa0c
Andreas Åkerlöf <a.akerlof@gmail.com>
2020-08-31 19:41:21 +0200

    feat(layout): added layout and empty pages

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui';
+
+import Layout from '../components/Layout';
+
+const Home = () => (
+  <Layout>
+    <div sx={{ paddingX: 7, paddingY: 8 }}>
+      <p>Home</p>
+    </div>
+  </Layout>
+);
+
+export default Home;
---
+++ src/pages/music.js
@@ -0,0 +1,14 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui';
+
+import Layout from '../components/Layout';
+
+const Music = () => (
+  <Layout>
+    <div sx={{ paddingX: 7, paddingY: 8 }}>
+      <p>Music</p>
+    </div>
+  </Layout>
+);
+
+export default Music;
