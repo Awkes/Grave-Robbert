d44ec70f3fdf5f52ccbe8ab4d8f214fb5e2156e3
Andreas Åkerlöf <a.akerlof@gmail.com>
2020-08-31 16:46:54 +0200

    feat(cms): set up datocms

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,14 @@
+require('dotenv').config({ path: '.env' });
+
 module.exports = {
   plugins: [
     'gatsby-plugin-theme-ui',
-    'gatsby-plugin-fontawesome-css'
+    'gatsby-plugin-fontawesome-css',
+    {
+      resolve: 'gatsby-source-datocms',
+      options: {
+        apiToken: process.env.API_TOKEN
+      }
+    }
   ],
 }
