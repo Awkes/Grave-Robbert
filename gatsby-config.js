793e1b855b2ce9c00822d3e669ddbde4c7f8f39a
Andreas Åkerlöf <a.akerlof@gmail.com>
2020-09-04 17:51:02 +0200

    feat(gatsby): added gatsby-source-filesystem and gatsby-transformer-yaml

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,14 @@ module.exports = {
     'gatsby-plugin-theme-ui',
     'gatsby-plugin-fontawesome-css',
     'gatsby-plugin-netlify-cms',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'data',
+        path: `${__dirname}/data/`
+      }
+    },
+    'gatsby-transformer-yaml',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
   ],
