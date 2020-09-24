afa50aa8699841d1ebd496c4e47af9f81fe675fb
Andreas Åkerlöf <a.akerlof@gmail.com>
2020-09-24 18:30:18 +0200

    fix(useWindowResize): added dependencies parameter

--- src/hooks/useWindowResize.js
+++ src/hooks/useWindowResize.js
@@ -1,8 +1,17 @@
 import React, { useEffect } from 'react';
 
-const useWindowResize = (handleResize) => {
-  if (typeof handleResize !== 'function') 
+/*
+  Usage: useWindowResize(callback, dependencies);
+
+  dependencies is optional and if used it's accessable
+  as the first argument in the callback function.
+*/
+
+const useWindowResize = (callback, dependencies = null) => {
+  if (typeof callback !== 'function') 
     throw('useWindowResize: You need to provide a callback function.');
+  
+  function handleResize() { callback(dependencies); }
 
   useEffect(() => {
     handleResize();
