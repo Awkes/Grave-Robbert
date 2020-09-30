a32e0d31b43e47a29fbf6605173ab4767d9d94bc
Andreas Åkerlöf <a.akerlof@gmail.com>
2020-09-30 12:31:31 +0200

    feat(frontpagenews): updated component with placeholder text

--- src/components/FrontpageNews.js
+++ src/components/FrontpageNews.js
@@ -2,34 +2,37 @@
 import PropTypes from 'prop-types';
 import { jsx } from 'theme-ui';
 
-import NewsCard from "./NewsCard";
+import Box from './Box';
+import NewsCard from './NewsCard';
 
-const FrontpageNews = ({ news }) => {
-  return news.length < 1 ? null : (
-    <div sx={{ 
-      marginY: 5,
-      display: 'grid',
-      gridTemplateRows: ['auto 1fr 1fr 1fr', null, '1fr 1fr 1fr'],
-      gridTemplateColumns: ['1fr', null, '1fr 1fr'],
-      alignItems: 'start',
-      gap: 4,
-      '&>*:first-of-type': { gridRow: [null, null, '1 / 4'] }
-    }}>
-      {news.map(({ id, date, image, title, excerpt, slug }, i) => (
-        <NewsCard 
-          date={date} 
-          excerpt={excerpt} 
-          image={image} 
-          invert={i === 2} 
-          key={id}
-          slug={slug}
-          small={i > 0}
-          title={title} 
-        />
-      ))}
-    </div>
-  );
-};
+const FrontpageNews = ({ news, noNews }) => news?.length < 1 ? (
+  <div sx={{ marginY: 5}}>
+    <Box>{noNews}</Box>
+  </div>
+) : (
+  <div sx={{ 
+    marginY: 5,
+    display: 'grid',
+    gridTemplateRows: ['auto 1fr 1fr 1fr', null, '1fr 1fr 1fr'],
+    gridTemplateColumns: ['1fr', null, '1fr 1fr'],
+    alignItems: 'start',
+    gap: 4,
+    '&>*:first-of-type': { gridRow: [null, null, '1 / 4'] }
+  }}>
+    {news.map(({ id, date, image, title, excerpt, slug }, i) => (
+      <NewsCard 
+        date={date} 
+        excerpt={excerpt} 
+        image={image} 
+        invert={i === 2} 
+        key={id}
+        slug={slug}
+        small={i > 0}
+        title={title} 
+      />
+    ))}
+  </div>
+);
 
 export default FrontpageNews;
 
@@ -44,8 +47,10 @@ FrontpageNews.propTypes = {
       slug: PropTypes.string,
     })
   ),
+  noNews: PropTypes.string,
 };
 
 FrontpageNews.defaultProps = {
   news: [],
+  noNews: 'There are no news yet!'
 };
